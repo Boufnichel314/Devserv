e117cb79c83526a6bdeeab904af675e22c44ab86
BOUGARRANI <93446329+BOUGARRANi@users.noreply.github.com>
2022-10-22 23:38:18 +0100

    Update

--- src/Component/Layout/Header.jsx
+++ src/Component/Layout/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import './Header.css'
+import logo from '../../Images/DevServ.png'
 export default function Header() {
   return (
-    <div className='header'>Header</div>
+    <div className='header'>
+        <div className='logo'>
+            <img src={logo} alt="" />
+        </div>
+    </div>
   )
 }
