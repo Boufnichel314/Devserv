401a65247a69ae125b9b46ef9147ea272743faca
BOUGARRANI <93446329+BOUGARRANi@users.noreply.github.com>
2022-10-22 23:19:32 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './Component/Layout/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <p>Testing new project !</p>
     </div>
   );
