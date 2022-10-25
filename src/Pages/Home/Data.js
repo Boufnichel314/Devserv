59f536cb19c93e528c3a5deba226a67732cae6a9
BOUGARRANI <93446329+BOUGARRANi@users.noreply.github.com>
2022-10-25 19:49:45 +0100

    Update

---
+++ src/Pages/Home/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    primary:'#',
+    topLine: 'DEVSERV',
+    headline: 'Création Site Internet',
+    description:
+      "Vous avez une idée ou un projet à réaliser sur internet? Vous souhaitez créer un nouveau site internet ou autre? Demandez nous un devis détaillé pour connaitre le prix de : Création site web, application mobile, logiciel, web design… Notre responsable vous contactera pour vous proposer les meilleures solutions qui seront à adaptées à vos besoins.",
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: require('../../Images_Services/test1.jpg'),
+    alt: 'Credit Card'
+  };
+  
+ 
